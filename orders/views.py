42bbcc648b03874e7b4dc7645d9dcd4e8048ca26
Érik Eiji <eriknibe1@gmail.com>
2023-05-05 14:30:43 -0300

    feat: add ProductOrder model

--- orders/views.py
+++ orders/views.py
@@ -4,31 +4,37 @@ from cart.models import Cart, ProductCart
 from .serializers import OrderSerializer
 from rest_framework.views import APIView, Response, Request, status
 
+
 class OrderView(APIView):
     def post(self, request: Request):
-        cart = Cart.objects.filter(user_id=request.user.id).first()
-        id = cart.id
-        cart_user = ProductCart.objects.filter(cart_id=id).first()
+        cart_user = ProductCart.objects.filter(cart=request.user.cart)
+        cart = Cart.objects.filter(user=request.user).first()
+
         sellers = []
+
         for elem in cart_user:
             if elem.products.seller not in sellers:
-                serializer = OrderSerializer(data=elem.products) 
-                sellers.append(elem.products.seller) 
-            
-            order = Order.objects.filter(user=request.user)   
+                serializer = OrderSerializer(data=elem.products)
+                sellers.append(elem.products.seller)
+
+            order = Order.objects.filter(user=request.user).first()
             order.products.add(elem.products)
+
+        for product in cart_user:
+            ...
+
         cart.delete()
         cart_user.delete()
-        
+
         serializer.is_valid(raise_exception=True)
 
         serializer.save(user=request.user)
-        
+
         return Response(serializer.data, status.HTTP_201_CREATED)
 
     def get(self, request: Request):
         order = Order.objects.filter(user=request.user)
 
         return Response(OrderSerializer(order).data)
-            
+
         ...
